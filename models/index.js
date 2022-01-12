b3bdf2f9ae51d2baeb09bf23617172448f45a698
DeathByDusk <elijahkanellakis@gmail.com>
2022-01-11 21:33:55 -0600

    populated models/index.js

--- models/index.js
+++ models/index.js
@@ -1,3 +1,19 @@
 const Post = require('./Post');
 const User = require('./User');
 const Comment = require('./Comment');
+
+Comment.belongsTo(User, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+});
+
+Post.belongsTo(User, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+});
+
+Post.hasMany(Comment, {
+    foreignKey: 'post_id'
+});
+
+module.exports = { User, Post, Comment };
